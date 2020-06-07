e870862559cec5e78b85aacfb52acf01b38065ee
‘Yonatan <yonatan@yaltman.com>
2020-06-07 19:36:12 +0300

    ng for

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -5,6 +5,8 @@ export enum EAuth {
   employee = 300,
   guest = 400
 }
+
+export type TLight = 'on' | 'off' | 'warmming';
 export type TStatus = 'active' | 'not-active';
 export interface IProduct {
   catalogId: string;
@@ -14,7 +16,65 @@ export interface IProduct {
   status: TStatus;
   img?: string;
 }
-
+const PATH = 'https://res.cloudinary.com/shufersal/image/upload/f_auto,q_auto/v1551800918/prod/product_images/products_medium/';
+const PRODUCTS: IProduct[] = [
+  {
+    catalogId: 'p0001',
+    name: 'מסיכת קורונה',
+    price: 100,
+    status: 'active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'TEV50_M_P_7296073381112_1.png'
+  },
+  {
+    catalogId: 'p0002',
+    name: 'אלקו ג\'ל',
+    price: 19,
+    status: 'active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'SYN66_M_P_7296073380948_1.png'
+  },
+  {
+    catalogId: 'p0002',
+    name: 'דליפקאן',
+    price: 24,
+    status: 'active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'TXY60_M_P_7290107647809_1.png'
+  },
+  {
+    catalogId: 'p0003',
+    name: 'שוגי',
+    price: 100,
+    status: 'active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'ZQN49_M_P_9992360704_1.png'
+  },
+  {
+    catalogId: 'p0004',
+    name: 'לואקר',
+    price: 54,
+    status: 'active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'DWI61_M_P_9992345983_1.png'
+  },
+  {
+    catalogId: 'p0005',
+    name: 'אלקו ג\'ל',
+    price: 16,
+    status: 'active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'SYN66_M_P_7296073380948_1.png'
+  },
+  {
+    catalogId: 'p0006',
+    name: 'דליפקאן',
+    price: 24,
+    status: 'not-active',
+    experationDate: new Date('01/01/25'),
+    img: PATH + 'TXY60_M_P_7290107647809_1.png'
+  },
+];
 
 @Component({
   selector: 'app-feed',
@@ -30,19 +90,14 @@ export class FeedComponent implements OnInit {
 
     search: 'אנא הקלד שם מוצר',
   }
+  light: TLight;
   btn = {
 
     search: 'חפש',
+    switch: 'כיבוי\\הדלקה'
   }
   filter = 'אוריה';
-  product: IProduct = {
-    catalogId: 'p0001',
-    name: 'מסיכת קורונה',
-    price: 100,
-    status: 'active',
-    experationDate: new Date('01/01/25'),
-    img: 'https://res.cloudinary.com/shufersal/image/upload/f_auto,q_auto/v1551800918/prod/product_images/products_medium/TEV50_M_P_7296073381112_1.png'
-  };
+  products = PRODUCTS;
 
   user;
   constructor() {
@@ -51,6 +106,7 @@ export class FeedComponent implements OnInit {
   }
 
   ngOnInit() {
+    // this.light = 'on';
   }
   onClick() {
     console.log(this.filter);
@@ -64,5 +120,22 @@ export class FeedComponent implements OnInit {
   }
   onBlur() {
     this.onClick();
+  }
+  changeLightState() {
+    switch (this.light) {
+      case 'off':
+        this.light = 'warmming';
+        break;
+      case 'warmming':
+        this.light = 'on';
+        break;
+      case 'on':
+        this.light = undefined;
+        break;
+
+      default:
+        this.light = 'warmming';
+        break;
+    }
   }
 }
